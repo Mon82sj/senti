f379de27d706c09dd537b44ede8e0b2abf66ea45
Mon82sj <monicasujatha824@gmail.com>
2024-08-13 14:19:00 +0530

    First commit

---
+++ client/build/precache-manifest.3c019533b3eaabacaa38f54f11ccd512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda0b252577128994048d58c1b192a37",
+    "url": "/Mon82sj/senti.git/index.html"
+  },
+  {
+    "revision": "23bd57c27998e19a6d7a",
+    "url": "/Mon82sj/senti.git/static/css/main.d7999535.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/Mon82sj/senti.git/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/Mon82sj/senti.git/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "23bd57c27998e19a6d7a",
+    "url": "/Mon82sj/senti.git/static/js/main.c1bdea75.chunk.js"
+  },
+  {
+    "revision": "6689262222923e30e5fe",
+    "url": "/Mon82sj/senti.git/static/js/runtime~main.74859f44.js"
+  }
+]);
